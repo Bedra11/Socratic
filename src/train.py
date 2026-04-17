fe1e294f5bf66a37e7bd450d83f07f935f39200a
Zeineb-ty <21042@supnum.mr>
2026-04-17 14:54:22 +0000

    feat(train): setup imports, config and MLflow connection

--- src/train.py
+++ src/train.py
@@ -1,1 +1,32 @@
-# train.py
+from dotenv import load_dotenv
+load_dotenv()
+
+import pandas as pd
+import pickle
+import os
+import yaml
+import mlflow
+import mlflow.sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    classification_report,
+    f1_score,
+    accuracy_score
+)
+import warnings
+warnings.filterwarnings("ignore")
+
+# CONFIG
+
+params      = yaml.safe_load(open("params.yaml"))
+eth_params  = params["train_ethics"]
+fal_params  = params["train_fallacy"]
+
+MLFLOW_URI  = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+
+mlflow.set_tracking_uri(MLFLOW_URI)
+print(f" MLflow tracking: {MLFLOW_URI}")
+
+os.makedirs("models", exist_ok=True)
